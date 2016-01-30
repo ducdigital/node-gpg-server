db4a895ee84299e9482e5d91e06fa39c6ea41593
Ta Duy Duc <duc@ducdigital.com>
2016-01-30 11:03:22 +0100

    API Change

--- GPGLib.js
+++ GPGLib.js
@@ -6,17 +6,38 @@ var crypto = require('crypto');
 var GPG = function (options) {
     options.tempKeyPath = options.tempKeyPath || __dirname + '/tmp/';
     options.defaultArgs = ['--with-colons', '--status-fd', '1'].concat(Array.isArray(options) ? options : []);
+    // Constant
+    const beginPubKey = '-----BEGIN PGP PUBLIC KEY BLOCK-----';
+    const endPubKey = '-----END PGP PUBLIC KEY BLOCK-----';
+    const gpgBBCODERegex = /\[gpg]([^]+?)\[\/gpg]/ig;
+
     // Private Methods
     var createPubKeyHash;
+    var replaceAll;
 
     // Public Methods
     var verify;
-    var parse;
+    var encrypt;
+    var parseResult;
+    var isGoodPubKeyFormat;
+    var getCleanPublicKey;
+    var parseBBCode;
+
+/*******************************************************************************
+ * Private Methods
+ ******************************************************************************/
 
     createPubKeyHash =  function (pubkey) {
       return crypto.createHash('sha1').update(pubkey).digest('hex');
     };
 
+    replaceAll = function (str, find, replace) {
+      return str.replace(new RegExp(find, 'g'), replace);
+    };
+
+/*******************************************************************************
+ * Pubic Methods
+ ******************************************************************************/
     verify = function (publicKey, signedMessage, cb) {
       async.waterfall ([
         function hash (next) {
@@ -51,10 +72,59 @@ var GPG = function (options) {
       });
     };
 
-    parse = function (gpgResp) {
-      if (!gpgResp || typeof gpgResp.toString !== 'function') {
-        return false;
+    encrypt = function (publicKey, message, cb) {
+      async.waterfall ([
+        function hash (next) {
+            let hash = createPubKeyHash(publicKey);
+            let tmpKeyRing = options.tempKeyPath + hash + '.gpg';
+            return next(null, tmpKeyRing);
+        },
+        function importKey (tmpKeyRing, next) {
+          gpg.importKey (publicKey,
+            options.defaultArgs.concat(['--no-default-keyring', '--keyring', tmpKeyRing]),
+            function (err, importResult) {
+              if (err) {
+                return next(err, tmpKeyRing);
+              }
+
+              return next(null, parseResult(importResult), tmpKeyRing);
+          });
+        },
+        function encryptMessage (importResult, tmpKeyRing, next) {
+          if (!importResult.importOK) {
+            return next(new Error('Key error'));
+          }
+
+          gpg.encrypt(message,
+            options.defaultArgs.concat(['--no-default-keyring', '--always-trust', '--armor', '--keyring', tmpKeyRing, '-r', importResult.id]),
+            function (err, result) {
+              if (err) {
+                return next(err, tmpKeyRing);
+              }
+
+              return next(null, tmpKeyRing, result);
+          });
+        },
+        function extractEncryptText (tmpKeyRing, result, next) {
+          let beginPart = '-----BEGIN PGP MESSAGE-----';
+          let endPart = '-----END PGP MESSAGE-----';
+          let encryptedMsg = result.toString();
+          let data = encryptedMsg.substring(
+            encryptedMsg.lastIndexOf(beginPart),
+            encryptedMsg.indexOf(endPart)
+          );
+          return next(null, data+endPart);
+        },
+      ], function done(err, result) {
+          return cb(err, result);
+      });
+    };
+
+    parseResult = function (gpgResp) {
+      if (!gpgResp || typeof gpgResp.toString !== 'function' || gpgResp instanceof Error) {
+        return new Error('Bad param: malformed Public Key');
       }
+
       let returnData = {
         'sigType': [],
         'signatureData': [],
@@ -92,13 +162,55 @@ var GPG = function (options) {
             returnData.sigType.push(line[0]);
             returnData['signatureData'] = line.slice(1, line.length).join(' ');
           }
+
+          if (line[0].indexOf('IMPORT_OK') > -1) {
+            returnData.importOK = true;
+            returnData.id = line[2];
+          }
         });
       return returnData;
     };
 
+    /**
+     * Verify public key format
+     */
+    isGoodPubKeyFormat = function (pubKey) {
+      let check = true;
+      check = check && (pubKey.indexOf(beginPubKey) > -1);
+      check = check && (pubKey.indexOf(endPubKey) > -1);
+
+      return check;
+    };
+
+    getCleanPublicKey = function (pubKey) {
+      return pubKey.substring(
+        pubKey.lastIndexOf(beginPubKey),
+        pubKey.indexOf(endPubKey)
+      ) + endPubKey;
+    };
+
+    parseBBCode = function (postContent) {
+      let br2nlStr = replaceAll(postContent, '<br />', '\n');
+      let results = [];
+      let matches;
+
+      while (matches = gpgBBCODERegex.exec(br2nlStr+br2nlStr)) {
+        results.push(matches[1]);
+      }
+
+      return results;
+    };
+
+/*******************************************************************************
+ * Lib returns
+ ******************************************************************************/
     return {
       verify: verify,
-      parse: parse
+      encrypt: encrypt,
+      parseResult: parseResult,
+      isGoodPubKeyFormat: isGoodPubKeyFormat,
+      getCleanPublicKey: getCleanPublicKey,
+      parseBBCode: parseBBCode,
     };
 };
 
--- index.js
+++ index.js
@@ -1,92 +1,136 @@
 'use strict';
-var GPGLib = require('./GPGLib');
-var fs = require("fs");
-var http = require('http');
+let GPGLib = require('./GPGLib');
+let express = require('express');
+let http = require('http');
+let async = require('async');
+let pgPromise = require('pg-promise')({});
+let _ = require('lodash');
 
-var publicKey1 = `
------BEGIN PGP PUBLIC KEY BLOCK-----
+const PORT = 8010;
 
-mQENBFamYggBCAC4hOItfTjKFSCHIauEOJipDKw9OSq3e5/UVtfTTgdu6hpd/2aZ
-pR/9RMoCscSgwzjvwzmRX6c7r4WBkzQqghyMAizWgmKwgb0AQiWCR0z4lHteHfho
-ZocKMI8Netvm0X5drL5F8qJpFxyjiA7reed4Vs5NM2I23IqXD1YNdWIzCDNH3/7T
-0KiDTh3II/JHYj5RL7WiyPucQmlE7beqc8U5WB7ptRfbQVZhy6AkRBr0fWy9si7l
-tZja0NYJgKG7L21cynxIj3bpGvpTKa83od5Z2Vlbfq3IuDn8TnADSEexIC07/0MU
-8Iot/KlnAoVB/Lb+9TpnJ8vP1lyXZ5Db1mq/ABEBAAG0QVZORnJlZW5ldCAoaHR0
-cDovL3ZuZnJlZW5ldHRleDdnZngub25pb24pIDx2bmZyZWVuZXRAc2lnYWludC5v
-cmc+iQE3BBMBCgAhBQJWpmIIAhsvBQsJCAcDBRUKCQgLBRYCAwEAAh4BAheAAAoJ
-EM7fhqyKYINrtTUIAKuOuFaPDK6snqxgWAm8WMgyUREklKlTcbwhEYX9Q03Pyp+x
-7/uG/P9U31T6A1/u2B+0gUSGQp7E5EYi6SEqIOMvjvUtEOPJ9YLgNvtQWuIOK8PO
-EQ4ByLFyrJridThj7z+JhvUrfpCzNmMKuYJgRAtcKmn1o2rOrGevnCur5uNDlAUI
-oEwy2ND21yeSeL1/riU7kgtt+/Jqc9LHo9SiKjQNzTyTk8LOxXo0k88WA6mnUClh
-+1euU1lpP590l0P5Fp0YLl8KIvClb5CV4xybCox2j5zyXYqeWtF/t7J8m15fHJS4
-qPQZ/Zm+w3tf1ROWqeRhbmIyxbNj7myrNaY8PAW5AQ0EVqZiCAEIAKPOhBQlzJle
-mLrDdfKp9VGSiC7wUXZhWlrLwI4qqsPhF1LTBICZBLhcy2x4bF2PdhXYWuSGWSci
-GCWM0/5K6wLPO/Gj54SglhTn/qj2p63mNC99W8lEr+ttHZ8Kg+ETEBK7oSwJN6LN
-1QPhJ83w5hbY6JyawwyBYL2ECFDekSIAad6vqnGC5lT0/6lodSBv7wRglXCxfBCe
-HyHffbHvWgjs7ZEBznMkbrMvQjX3TgvK8jgLImIPs8R1ZBv601iQFbMBAWlvRAbO
-XRpgDUJJSW1HT+qXkb7vAcnaRAuevsO3FtInlHJ7nI1yTSQlSR5A2GnqbHSP7Omd
-rj05jbJSt4cAEQEAAYkCngQYAQoACQUCVqZiCAIbLgGJCRDO34asimCDa8C9IAQZ
-AQoAZgUCVqZiCF8UgAAAAAAuAChpc3N1ZXItZnByQG5vdGF0aW9ucy5vcGVucGdw
-LmZpZnRoaG9yc2VtYW4ubmV0MTY5QjRFQzU3RTAwMDRGMUZGQkEzRjhDQzFGNzdC
-MUEyRjRBNkUzOQAKCRDB93saL0puOSP3CACZ6dCgEi6iBbie7jYkrVMczFMvVFbc
-P0QqNq++aHXfZdAZTkIKGXCJfVQtPZyXOGxIbtH5W7Vp9H1qd+CqujqyIMWvvalj
-2QhemqhEH41J8hpxnSVc80KYF2i/Wq+j6jgkAPRveebBMNUAaxUo8Kcmz7o0RNWJ
-V/8vlHgJmQq1l7WXSPt5W7zGoB/SD+2X9dSFjVxXFbjfANGFl97LBQSydxwNpuTW
-L15m8b2kzC+wPmfmHPWv1ReHiqK+GQhskGBflo7kRBZ6S/OKOAqkIpw15ct4EDQp
-euG7jbOa0wKbZjC4cq3sBA8Tcvki57pOHEFOfTHX59atZuugbhC2jPbdkqAH/i0Q
-e3ZBlMDJKEDrr1JStllIfp4OGpM8KWeeVl4wrIjOyeZu4EYEV7JjyQAZi9M5o135
-Ynjr9Q95APiPjdE7tDKDLZ4z8qC8YXlSu2Y4WbFIQpDLwVzXtcOKCdpjvvd3eGFi
-R9Hpll1eaPoV/+ulGzFubnA5QhdK7x7mVFgfEO9zIvUiI2rSHjaTFBOlmbnqzvlO
-Zek7ulwi9+4E1Pm2deMX3qsViwipVUUdOPNY8ESXTpJqyMwS3oSEM7tyhEZigPcC
-DkK+78WuMyawdPdb61cG9uc61IsfK7ZhUMbxYKuzkPL7qFdqhJDnK0cc1cCutEEj
-4GcfXGZ30g5/trk9NIw=
-=kXtJ
------END PGP PUBLIC KEY BLOCK-----
-`;
+let conString = "postgres://vnfreenet@localhost/vnfreenet";
+let app = express();
 
-var signedMessage1 = `
------BEGIN PGP SIGNED MESSAGE-----
-Hash: SHA512
+let coreApp = {
+  verify: function (req, res, next) {
+    let postId = _.toInteger(req.params.postId);
+    let gpgIndex = _.toInteger(req.params.index);
+    let query = "SELECT t.value as pgp_key, m.body as content FROM vfn_themes as t INNER JOIN vfn_messages as m ON t.id_member = m.id_member WHERE m.id_msg = ${postId} AND t.variable = 'cust_m'";
 
-Testing GPG Key signing
------BEGIN PGP SIGNATURE-----
-
-iQF8BAEBCgBmBQJWpmRuXxSAAAAAAC4AKGlzc3Vlci1mcHJAbm90YXRpb25zLm9w
-ZW5wZ3AuZmlmdGhob3JzZW1hbi5uZXQxNjlCNEVDNTdFMDAwNEYxRkZCQTNGOEND
-MUY3N0IxQTJGNEE2RTM5AAoJEMH3exovSm45ewwIAIlYcto/WyuVXE478OPQnQA2
-VrCB4Z/dqkNEP3Jpko6Qlp4Ud95XERxZkgHC2AE2D6UiwdZOGv/nDoopiAmPfms+
-7cnB7EkM4yZOQHbcNBw87uSzrnevljFIHyNy8/Exngkckn3HTLfApMdBhhHlL7Y1
-8m7svs720zInVPqek11UwyXpjJ0qVPq+HIj2cM7O2h9SxYMbv1zhRe4MD51Lu1Pe
-b0GvMYMxBcYTqdpk04hP2DGoMaLuLSoUSTQ3HmuY5iUgPj2/HUVmL4XKyf7kcGTO
-pBCh7uhYBVfY99Zu/9KsJ9lTlcB1AUhz8JkIZb+pTmqoZbhb3RR0V+b3lStravU=
-=/8NV
------END PGP SIGNATURE-----
-`;
-
-var gpgLib = new GPGLib({
-  tempKeyPath: __dirname + '/tmp/'
-});
-var pg = require('pg');
-var conString = "postgres://vnfreenet:x8**213hxa1@#%61@10.125.125.12/vnfreenet";
-
-async.parallel([
-  function DBConnection (next) {
-    pg.connect(conString, function(err, client, done) {
-      return next(err, {client: client, done: done});
-    });
-  },
-  function Server (next) {
-    http.createServer(next).listen(8010);;
-  }
-], function done (err, result){
-  return gpgLib.verify(publicKey1, signedMessage1, function (err, result) {
-    result.Server.res.writeHead(200, {'Content-Type': 'application/json'});
-    if (!!err) {
-      result.Server.res.write(JSON.stringify(gpgLib.parse(err)));
-    } else {
-      result.Server.res.write(JSON.stringify(gpgLib.parse(result)));
+    if (postId < 1) {
+      return next (new Error('Bad Param: postId'));
     }
 
-    result.Server.res.end();
-  });
+    async.waterfall([
+      function findDb (next) {
+        app.db.one(query, {
+          postId: postId
+        }).then(function (result) {
+          return next(null, result);
+        }).catch(function (err) {
+          return next(err);
+        });
+      },
+      function parsePostData (postData, next) {
+        let parsed = {};
+
+        if (!app.gpg.isGoodPubKeyFormat(postData.pgp_key)) {
+          return next(new Error('Public Key is incorrect format'));
+        }
+
+        parsed.publicKey = app.gpg.getCleanPublicKey(postData.pgp_key);
+        parsed.signedMessages = app.gpg.parseBBCode(postData.content);
+
+        return next(null, parsed);
+      },
+      function verifySignature (pData, next) {
+        let signedMessage = _.get(pData, 'signedMessages['+gpgIndex+']');
+
+        if (!signedMessage) {
+          return next(new Error('No signed message found'));
+        }
+
+        return app.gpg.verify(pData.publicKey, signedMessage, next);
+      }
+    ], function done (err, gpgVerifyResult) {
+      if (err) {
+        res.status(406);
+        return next(app.gpg.parseResult(err));
+      }
+
+      return res.json(app.gpg.parseResult(gpgVerifyResult));
+    });
+  },
+  encrypt: function (req, res, next) {
+    let userId = _.toInteger(req.params.userId);
+    let message = req.query.message;
+    let query = "SELECT t.value as pgp_key FROM vfn_themes as t WHERE t.id_member = ${userId} AND t.variable = 'cust_m'";
+
+    if (userId < 1) {
+      return next (new Error('Bad Param: userId'));
+    }
+
+    async.waterfall([
+      function findDb (next) {
+        app.db.one(query, {
+          userId: userId
+        }).then(function (result) {
+          return next(null, result);
+        }).catch(function (err) {
+          return next(err);
+        });
+      },
+      function parseData (postData, next) {
+        let parsed = {};
+
+        if (!app.gpg.isGoodPubKeyFormat(postData.pgp_key)) {
+          return next(new Error('Public Key is incorrect format'));
+        }
+
+        parsed.publicKey = app.gpg.getCleanPublicKey(postData.pgp_key);
+
+        return next(null, parsed);
+      },
+      function encrypt (parsed, next) {
+        return app.gpg.encrypt(parsed.publicKey, message, next);
+      }
+    ], function (err, result) {
+      console.log(err);
+      res.send(result);
+    });
+  },
+}
+
+app.get('/verify/:postId/:index', coreApp.verify);
+app.get('/verify/:postId', coreApp.verify);
+/**
+ * /encrypt/:userId?message={string}
+ */
+app.get('/encrypt/:userId', coreApp.encrypt);
+
+async.parallel({
+  DBConnection: function (next) {
+    return next(null, pgPromise(conString));
+  },
+  GPG: function (next) {
+    let gpgLib = new GPGLib({
+      tempKeyPath: __dirname + '/tmp/'
+    });
+
+    return next(null, gpgLib);
+  },
+  Server: function (next) {
+    console.info('[x] Server started @ ' + PORT + '...');
+    let httpServer = http.createServer(app).listen(PORT);
+
+    return next(null, httpServer);
+  },
+}, function done (err, result){
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.info('[x] App initialized');
+  app.db = result.DBConnection;
+  app.gpg = result.GPG;
 });
